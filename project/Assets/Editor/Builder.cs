9f38eb2d062ddf94a9a8e1d102231f268125acb4
Aaron Cox <aaronrcox123@gmail.com>
2020-09-21 10:26:07 +1000

    updated build script

--- project/Assets/Editor/Builder.cs
+++ project/Assets/Editor/Builder.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using System.Linq;
+using System;
+using System.Threading;
 
 public class Builder
 {
     public static void Build()
     {
         string assetFolderPath = Application.dataPath;
-        string filename = assetFolderPath + "/../builds/MyGame.exe";
+        string pcFileName = assetFolderPath + "/../builds/pc/MyGame.exe";
+        string webFileName = assetFolderPath + "/../builds/web/";
 
         var scenes = EditorBuildSettings.scenes;
         var levels = scenes.Select(z => z.path).ToArray();
 
-        BuildPipeline.BuildPlayer(levels, filename, BuildTarget.StandaloneWindows, BuildOptions.None);
+        BuildPipeline.BuildPlayer(levels, webFileName, BuildTarget.WebGL, BuildOptions.None);
+        BuildPipeline.BuildPlayer(levels, pcFileName, BuildTarget.StandaloneWindows, BuildOptions.None);
+        
+        Thread.Sleep(2000);
     }
 }
